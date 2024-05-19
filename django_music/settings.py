85cc0ef2a1df901732a9d04a8c0f48d1b26c11e7
fikriwahab <fikrimassaidwahab2@gmail.com>
2024-05-19 19:34:20 +0700

    Perubahan Settings Deployment 2

--- django_music/settings.py
+++ django_music/settings.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
