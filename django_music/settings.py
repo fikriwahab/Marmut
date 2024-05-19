ad24317839129414ba1f3c15679b54fb82816d5b
fikriwahab <fikrimassaidwahab2@gmail.com>
2024-05-19 19:17:27 +0700

    Perubahan Settings Deployment

--- django_music/settings.py
+++ django_music/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uorfeip09)+_+xu^t%)r^u%d7@&eh*x8=(*_h%gv9_!s&c*o42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
